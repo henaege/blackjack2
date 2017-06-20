7c33d0968926ff32cf24edc59fafb8e1564b100a
henaege <henaege@gmail.com>
2017-06-19 22:06:15 -0400

    fixed player blackjack win condition, disable hit button when needed, first dealer card starts face down

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,11 @@ $(document).ready(function() {
 
     const freshDeck = createDeck();
     let playersHand = [];
+    let playersTotal = 0;
     let dealersHand = [];
     let theDeck = freshDeck.slice();
     let standing = false;
-    letPlayerCash = 500;
+    let PlayerCash = 500;
     let bet = 0;
     ////////////////////
     // EVENT HANDLERS//
@@ -18,35 +19,11 @@ $(document).ready(function() {
         // Deal Cards...
     $('.deal-button').click(function() {
         reset();
+        // playersTotal = 0;
         // Deck is now shuffled, add the 1st and 3rd cards to the player's hand and the DOM. Do the same for the dealer with 2nd and 4th cards
-        playersHand.push(theDeck.shift()) //top card -> player's hand
-        dealersHand.push(theDeck.shift()) //(next) top card -> dealer's hand
-        playersHand.push(theDeck.shift()) //(next) top card -> player's hand
-        dealersHand.push(theDeck.shift()) //(next) top card -> dealer's hand
-
-        // update the DOM with images
-        // placeCard('player', 1, playersHand[0]);
-        // placeCard('player', 2, playersHand[1]);
-
-        // placeCard('dealer', 1, dealersHand[0]);
-        // placeCard('dealer', 2, dealersHand[1]);
         
-        setTimeout(function(){placeCard('player',1,playersHand[0]);},200)
-        setTimeout(function(){placeCard('player',2,playersHand[1]);},1400)
-        setTimeout(function(){placeCard('dealer',1,dealersHand[0]);},600)
-        setTimeout(function(){placeCard('dealer',2,dealersHand[1]);},1800)
-
-        setTimeout(function(){calculateTotal(playersHand, 'player');},2600)
-        setTimeout(function(){calculateTotal(dealersHand, 'dealer');},2600)
-        if (standing == true){
-            checkWin();
-        }
-        if (calculateTotal(playersHand, 'player') >= 21) {
-            checkWin();
-        }
-        if (calculateTotal(dealersHand, 'dealer') >= 21) {
-            checkWin();
-        }
+        // setTimeout(function(){calculateTotal(dealersHand, 'dealer');},2600)
+       
     });
 
         // Hit Player with ClipboardEvent.apply.apply.
@@ -62,10 +39,12 @@ $(document).ready(function() {
             let slotForNewCard = playersHand.length;
             placeCard('player', slotForNewCard, playersHand[lastCardIndex]);
             calculateTotal(playersHand, 'player');
+            // checkWin();
         } 
         if (calculateTotal(playersHand, 'player') >= 21) {
             checkWin();
         }
+        
     });
         // Player stands...
     $('.stand-button').click(function() {
@@ -75,11 +54,13 @@ $(document).ready(function() {
             dealersHand.push(theDeck.shift());
             let lastCardIndex = dealersHand.length - 1;
             let slotForNewCard = dealersHand.length;
-            setTimeout(function(){placeCard('dealer', slotForNewCard, dealersHand[lastCardIndex]);}, 600);
-            dealerTotal = setTimeout(function(){calculateTotal(dealersHand, 'dealer')}, 600);
+            dealerTotal = calculateTotal(dealersHand, 'dealer');
+            placeCard('dealer', slotForNewCard, dealersHand[lastCardIndex]);
+            
         }
-        standing = true;
+        $('.hit-button').prop("disabled", true);
         checkWin();
+
 
     });
 
@@ -94,22 +75,29 @@ $(document).ready(function() {
         if (playerTotal > 21) {
             winner = "You busted! The House wins!"
         }else if (playerTotal == 21){
+            $('.hit-button').prop("disabled", true);
             winner = "Blackjack! You win!"
         } else if (dealerTotal > 21) {
+            $('.hit-button').prop("disabled", true);
             winner = "The House busted, you Win!!";
         } else {
             if ((playerTotal > dealerTotal) && (dealerTotal < 21)) {
+                $('.hit-button').prop("disabled", true);
                 winner = "You beat the House!";
             } else if (dealerTotal == 21) {
                 winner = "Blackjack! The House wins"
             } else if (playerTotal < dealerTotal) {
                 winner = "You lose!";
             } else {
+                $('.hit-button').prop("disabled", true);
                 winner = "It's a push.";
             }
         }
+        console.log(dealersHand);
+        $('#dealercard1').html('<img src="./cards/' + dealersHand[0] + '.png">')
         $('.message').text(winner);
-    }
+    };
+
     function createDeck() {
         let newDeck = [];
         let suits = ['h', 's', 'd', 'c'];
@@ -135,10 +123,20 @@ $(document).ready(function() {
     function placeCard(who, where, what) {
         // Find the DOM element based on the args we want to change
         // (find the element we want to put the image in)
-        let slotForCard = '.' + who + '-cards .card-' + where;
+        if (who == 'dealer' && where == 1) {
+            var slotForCard = '.' + who + '-cards .card-' + where;
+        imgTag = '<img src="./cards/deck.png">';
+        }else {
+            var slotForCard = '.' + who + '-cards .card-' + where;
         imgTag = '<img src="./cards/' + what + '.png">';
+        }
+        
         $(slotForCard).html(imgTag);
         $(slotForCard).addClass('dealt');
+
+        // if (calculateTotal(playersHand, 'player') == 21) {
+        //     checkWin();
+            // }
     }
 
     function calculateTotal(hand, who) {
@@ -161,12 +159,13 @@ $(document).ready(function() {
         for (let i = 0; i < totalAces; i++) {
             if (totalHandValue > 21)  {
             totalHandValue -= 10;
-        }
+            }
         }
         
         // We have the total, now update the DOM
         let totalUpdate = '.' + who + '-total-number';
         $(totalUpdate).text(totalHandValue);
+        // checkWin();
         return totalHandValue;
     }
 
@@ -181,12 +180,53 @@ $(document).ready(function() {
         // 3. rest cards in the DOM
         $('.card').html('');
         // // 4. reset totals for player and dealer
-        $('.dealer-total-number').html(' 0');
+        $('.dealer-total-number').html(' ?');
         $('.player-total-number').html(' 0');
         $('.message').text('');
         $('.card').removeClass('dealt');
         $('.card').removeClass('dealt');
-        standing = false;
+        $('.hit-button').prop("disabled", false);
+        playersHand.push(theDeck.shift()) //top card -> player's hand
+        dealersHand.push(theDeck.shift()) //(next) top card -> dealer's hand
+        playersHand.push(theDeck.shift()) //(next) top card -> player's hand
+        dealersHand.push(theDeck.shift()) //(next) top card -> dealer's hand
+
+        // update the DOM with images
+        // placeCard('player', 1, playersHand[0]);
+        // placeCard('player', 2, playersHand[1]);
+
+        // placeCard('dealer', 1, dealersHand[0]);
+        // placeCard('dealer', 2, dealersHand[1]);
+        
+        setTimeout(function(){placeCard('player',1,playersHand[0]);},200)
+        setTimeout(function(){placeCard('player',2,playersHand[1]);},1400)
+        setTimeout(function(){placeCard('dealer',1,dealersHand[0]);},600)
+        setTimeout(function(){placeCard('dealer',2,dealersHand[1]);},1800)
+        playersTotal = setTimeout(function(){calculateTotal(playersHand, 'player');},2400);
+        let totalHandValue = 0;
+        let thisCardValue = 0;
+        let totalAces = 0;
+        for (i = 0; i < playersHand.length; i++) {
+            thisCardValue = Number(playersHand[i].slice(0, -1));
+            if (thisCardValue > 10) {
+                thisCardValue = 10;
+            }else if (thisCardValue == 1) {
+                totalAces += 1;
+                thisCardValue = 11;
+            }
+            totalHandValue += thisCardValue;
+            
+        }
+        for (let i = 0; i < totalAces; i++) {
+            if (totalHandValue > 21)  {
+            totalHandValue -= 10;
+            }
+        }
+        console.log(totalHandValue)
+        if (totalHandValue == 21) {
+                setTimeout(()=>{checkWin();}, 2500);
+            }
+        // if(calculateTotal(playersHand, ))
     }
 
 });
